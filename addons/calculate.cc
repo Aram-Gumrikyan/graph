ab254f54b66fb38b8970415096116412bc526936
Aram <aram.gumrikyan.01@gmail.com>
2022-04-20 23:53:46 +0400

    fix: add correct addons file

---
+++ addons/calculate.cc
@@ -0,0 +1,35 @@
+#include <node.h>
+#include <iostream>
+
+using namespace std;
+
+namespace calculate
+{
+  using v8::Array;
+  using v8::FunctionCallbackInfo;
+  using v8::Isolate;
+  using v8::Local;
+  using v8::Number;
+  using v8::Object;
+  using v8::String;
+  using v8::Value;
+
+  void Sum(const FunctionCallbackInfo<Value> &args)
+  {
+    Isolate *isolate = args.GetIsolate();
+
+    int value = args[0].As<Array>()->Value();
+
+    cout << value[0] << endl;
+
+    Local<Number> a = Number::New(isolate, 20);
+    args.GetReturnValue().Set(a);
+  }
+
+  void Initialize(Local<Object> exports)
+  {
+    NODE_SET_METHOD(exports, "calc", Sum);
+  }
+
+  NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize);
+}
